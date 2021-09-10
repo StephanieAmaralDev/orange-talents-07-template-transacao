c8bd89b9de863bd840f4bce74417eb0a46e2ad60
Stephanie dos Santos Amaral <stephanie.amaral@zup.com.br>
2021-09-10 10:11:30 -0300

    Autenticação e autorização

---
+++ src/main/java/br/com/zup/edu/stephanie/config/SegurancaConfig.java
@@ -0,0 +1,23 @@
+package br.com.zup.edu.stephanie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+public class SegurancaConfig {
+
+    @Configuration
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests(authorizeRequests ->
+                    authorizeRequests.antMatchers(HttpMethod.GET, "/transacoes/**").hasAuthority("SCOPE_allgranted")
+                            .anyRequest().authenticated()
+            ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        }
+    }
+}
+
